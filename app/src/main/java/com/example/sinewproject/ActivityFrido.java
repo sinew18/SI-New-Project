58b57a8ae8f9113ef48125143bb9f0871c94b025
Frido <archalois3004@gmail.com>
2020-09-23 09:56:51 +0700

    tes

---
+++ app/src/main/java/com/example/sinewproject/ActivityFrido.java
@@ -0,0 +1,14 @@
+package com.example.sinewproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityFrido extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frido);
+    }
+}
